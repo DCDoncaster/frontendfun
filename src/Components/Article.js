5e9b6b85c184439f28cb1183a2728f3b195adb1f
David Clarke <david26th@gmail.com>
2023-05-18 16:58:34 +0100

    Front End Changes

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Container, Image, Row, Col } from "react-bootstrap";
 
-function Article(props){
-    return(
-        <article>
-<a href={'https://reddit.com'+props.article.permalink} target = "_blank">
-    <h3>{props.article.title}</h3>
-</a>
-        </article>
-    )
+function Article(props) {
+  return (
+    <article>
+      <Container>
+        <Row>
+          <Col md="auto" style={{width:"15%"}}>
+            {props.article.thumbnail[0] == "h" ? (
+              <Image src={props.article.thumbnail} />
+            ) : (
+              ""
+            )}
+          </Col>
+          <Col>
+            <a
+              href={"https://reddit.com" + props.article.permalink}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h3>{props.article.title}</h3>
+              <a href={"https://reddit.com/u/"+props.article.author} target="_blank" rel="noreferrer">{props.article.author}</a>
+            </a>  
+          </Col>
+        </Row>
+      </Container>
+    </article>
+  );
 }
 
 export default Article;
--- src/Components/Reader.js
+++ src/Components/Reader.js
@@ -5,8 +5,9 @@ import Article from './Article';
 function Reader() {
   const [articles, setArticles]=useState([]);
   const [subreddit, setSubreddit]=useState('frontend');
-
-useEffect(()=>{
+  const [subredditTitle, setSubredditTitle]=useState('')
+//get subreddit posts 
+  useEffect(()=>{
 fetch("https://www.reddit.com/r/"+ subreddit +".json").then(res=>{
   if(res.status !== 200){
     //TODO: Show a banner if things don't work
@@ -25,6 +26,28 @@ if(data != null){
   console.log(error);
 })
 }, [subreddit]);
+//get community info
+useEffect(()=>{
+  fetch("https://www.reddit.com/r/"+ subreddit +"/about.json").then(res=>{
+    if(res.status !== 200){
+      //TODO: Show a banner if things don't work
+      console.log("Error: " + res.status);
+      return;
+    }
+    res.json().then(data=>{
+  if(data != null){
+    
+    // setSubredditIcon(data.community_icon);//pass articles to the state
+    console.log(data.data.title)
+    setSubredditTitle(data.data.title)
+     
+  };
+    });
+  })
+  .catch(error=>{
+    console.log(error);
+  })
+  }, [subreddit]);
 
 
   return (
@@ -32,10 +55,11 @@ if(data != null){
   <div className='Reader'>
     <header className="Reader-header">
       <input type="text" className="input" value={subreddit} onChange={e => setSubreddit(e.target.value)}/>
-      </header>
+      <p>{subredditTitle}</p>
+            </header>
       <div className="articles">
         {
-          (articles !== null) ? articles.map((article,index) => <Article key={index} article={article.data}/>):''
+          (articles !== null) ? articles.map((article,index) => <Article key={index}image={article.thumbnail} article={article.data}/>):''
         }
       </div>
   </div>
