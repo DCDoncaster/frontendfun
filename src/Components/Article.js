993ac00d85ebc7a0b07d2c483d6685bffc9632ee
David Clarke <david26th@gmail.com>
2023-06-01 15:52:56 +0100

    Refactoring of Reader Design

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,36 +1,68 @@
 import React from "react";
 import { Container, Image, Row, Col, Accordion } from "react-bootstrap";
+import AccordionBody from "react-bootstrap/esm/AccordionBody";
 
 function Article(props) {
   return (
     <article>
       <Container>
         <Row>
-          {(props.article.over_18 === false)?
-          <><Col md="auto" style={{width:"15%"}}>
-            {props.article.thumbnail[0] === "h" ? (
-              <Image src={props.article.thumbnail} />
-            ) : (
-              <Image src="https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png" fluid/>
-            )}
-          </Col>
-          <Col>
-          <Accordion defaultActiveKey="0">
-<Accordion.Item eventKey={props.index} flush>
-<Accordion.Header>
-<a
-              href={"https://reddit.com" + props.article.permalink}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {props.article.title}</a>
-</Accordion.Header>
-<Accordion.Body>{props.article.selftext} <br></br><br></br>Author: <a href={"https://reddit.com/u/"+props.article.author} target="_blank" rel="noreferrer">{props.article.author}</a></Accordion.Body>
-</Accordion.Item>
-
-          </Accordion>
-
-          </Col></>:''}
+          {props.article.over_18 === false ? (
+            <>
+              <Col md="auto" style={{ width: "15%" }}>
+                {props.article.thumbnail[0] === "h" ? (
+                  <Image src={props.article.thumbnail} />
+                ) : (
+                  <Image
+                    src="https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png"
+                    fluid
+                  />
+                )}
+              </Col>
+              <Col>
+                <Accordion defaultActiveKey="0">
+                  <Accordion.Item eventKey={props.index} flush>
+                    <Accordion.Header>
+                      <Col>
+                        <Row>
+                          {" "}
+                          <a
+                            href={
+                              "https://reddit.com" + props.article.permalink
+                            }
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            {props.article.title}
+                          </a>
+                        </Row>
+                        <Row></Row>
+                        <Row>
+                          <Col>{props.article.link_flair_text}</Col>
+                        </Row>
+                      </Col>
+                      <Col>
+                        Author:{" "}
+                        <a
+                          href={"https://reddit.com/u/" + props.article.author}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          {props.article.author}
+                        </a>
+                      </Col>
+                    </Accordion.Header>
+                    {props.article.selftext !== null?<>
+                    <AccordionBody>{props.article.selftext}</AccordionBody>
+                    <AccordionBody>Upvotes: {props.article.ups}</AccordionBody></>
+                    :""}
+                  </Accordion.Item>
+                </Accordion>
+              </Col>
+            </>
+          ) : (
+            ""
+          )}
         </Row>
       </Container>
     </article>
--- src/Components/Reader.js
+++ src/Components/Reader.js
@@ -1,66 +1,77 @@
-import React, {useState,useEffect} from 'react';
-import Article from './Article';
-
+import React, { useState, useEffect } from "react";
+import Article from "./Article";
 
 function Reader() {
-  const [articles, setArticles]=useState([]);
-  const [subreddit, setSubreddit]=useState('frontend');
-  const [subredditTitle, setSubredditTitle]=useState('')
-//get subreddit posts 
-  useEffect(()=>{
-fetch("https://www.reddit.com/r/"+ subreddit +".json").then(res=>{
-  if(res.status !== 200){
-    //TODO: Show a banner if things don't work
-    console.log("Error: " + res.status);
-    return;
-  }
-  res.json().then(data=>{
-if(data != null){
-  console.log("Data Received");
-  setArticles(data.data.children);//pass articles to the state
-   
-};
-  });
-})
-.catch(error=>{
-  console.log(error);
-})
-}, [subreddit]);
-//get community info
-useEffect(()=>{
-  fetch("https://www.reddit.com/r/"+ subreddit +"/about.json").then(res=>{
-    if(res.status !== 200){
-      //TODO: Show a banner if things don't work
-      console.log("Error: " + res.status);
-      return;
-    }
-    res.json().then(data=>{
-  if(data != null){
-        console.log(data.data.title)
-      setSubredditTitle((data.data.over18 === false) ? data.data.title : "You shouldn't be looking at this at work")
-     
-  };
-    });
-  })
-  .catch(error=>{
-    console.log(error);
-  })
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState("frontend");
+  const [subredditTitle, setSubredditTitle] = useState("");
+  //get subreddit posts
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/" + subreddit + ".json")
+      .then((res) => {
+        if (res.status !== 200) {
+          //TODO: Show a banner if things don't work
+          console.log("Error: " + res.status);
+          return;
+        }
+        res.json().then((data) => {
+          if (data != null) {
+            setArticles(data.data.children); //pass articles to the state
+          }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [subreddit]);
+  //get community info
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/" + subreddit + "/about.json")
+      .then((res) => {
+        if (res.status !== 200) {
+          //TODO: Show a banner if things don't work
+          console.log("Error: " + res.status);
+          return;
+        }
+        res.json().then((data) => {
+          if (data != null) {
+            console.log(data.data.title);
+            setSubredditTitle(
+              data.data.over18 === false
+                ? data.data.title
+                : "You shouldn't be looking at this at work"
+            );
+          }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [subreddit]);
 
-
   return (
-
-  <div className='Reader'>
-    <header className="Reader-header">
-      <input type="text" className="input" value={subreddit} onChange={e => setSubreddit(e.target.value)}/>
-      <p>{subredditTitle}</p>
-            </header>
+    <div className="Reader">
+      <header className="Reader-header">
+        <input
+          type="text"
+          className="input"
+          value={subreddit}
+          onChange={(e) => setSubreddit(e.target.value)}
+        />
+        <p>{subredditTitle}</p>
+      </header>
       <div className="articles">
-        {
-          (articles !== null) ? articles.map((article,index) => <Article key={index}image={article.thumbnail} article={article.data}/>):''
-        }
+        {articles !== null
+          ? articles.map((article, index) => (
+              <Article
+                key={index}
+                image={article.thumbnail}
+                article={article.data}
+              />
+            ))
+          : ""}
       </div>
-  </div>
+    </div>
   );
 }
 
